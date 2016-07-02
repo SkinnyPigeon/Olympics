a910be35a30278a9bc69d48a18aad82b5d2cb8e4
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-07-02 12:24:57 +0100

    most functions added and working to nations

--- models/nation.rb
+++ models/nation.rb
@@ -1,3 +1,6 @@
+require('pry-byebug')
+require_relative('../db/sql_runner')
+
 class Nation
 
   attr_reader( :name, :id, :points )
@@ -8,4 +11,51 @@ class Nation
     @points = 0
   end
 
+
+  def save()
+    sql = "INSERT INTO nations ( name ) VALUES ('#{@name}') RETURNING *"
+    return Nation.map_item( sql )
+  end
+
+  def self.all()
+    sql = "SELECT * FROM nations"
+    return Nation.map_items( sql )
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM nations"
+    run_sql( sql )
+  end
+
+  def self.map_items( sql )
+    nation = run_sql( sql )
+    result = nation.map { |product| Nation.new( product ) }
+    return result
+  end
+
+  def self.map_item( sql )
+    result = Nation.map_items( sql )
+    return result.first
+  end
+
+  def self.find( id )
+    sql = "SELECT * FROM nations WHERE id = #{id}"
+    return Nation.map_item( sql )
+  end
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
