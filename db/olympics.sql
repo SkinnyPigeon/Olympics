2e4b66c53793065642bb637a440d922cd1d374a4
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-07-02 11:50:59 +0100

    tables added

---
+++ db/olympics.sql
@@ -0,0 +1,28 @@
+DROP TABLE athletes_events;
+DROP TABLE events;
+DROP TABLE athletes;
+DROP TABLE nations;
+
+CREATE TABLE nations (
+  id SERIAL4 primary key,
+  name VARCHAR(255),
+  total_points INT4
+);
+
+CREATE TABLE athletes (
+  id SERIAL4 primary key,
+  name VARCHAR(255),
+  nation_id INT4 references nations(id)
+);
+
+CREATE TABLE events (
+  id SERIAL4 primary key,
+  name VARCHAR(255)
+);
+
+CREATE TABLE athletes_events (
+  id SERIAL4 primary key,
+  athlete_id INT4 references athletes(id),
+  event_id INT4 references events(id),
+  position_id INT4
+);
